68cada7f6ab586b0052038f611b635ed7f635bc4
Minolie <minolifdo@hotmail.com>
2016-05-12 23:42:04 +0530

    Add files via upload

---
+++ Brain Game/src/MainMenu.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Minolie on 2/10/2016.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainMenu extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
